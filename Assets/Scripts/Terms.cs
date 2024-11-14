523a933ceae1ffccb1747d821231ba77d2dd85b7
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-11-14 16:49:15 -0300

    feat: add terms

--- Assets/Scripts/Terms.cs
+++ Assets/Scripts/Terms.cs
@@ -12,9 +12,9 @@ public class Terms : MonoBehaviour
 
     void OnEnable()
     {
-        myToggle.isOn = false;
-        UpdateButtonState(false);
-        myToggle.onValueChanged.AddListener(OnToggleValueChanged);
+        // myToggle.isOn = false;
+        // UpdateButtonState(false);
+        // myToggle.onValueChanged.AddListener(OnToggleValueChanged);
 
         myButton.onClick.AddListener(OnButtonClicked);
     }
@@ -24,21 +24,21 @@ public class Terms : MonoBehaviour
         fitas_terms.gameObject.SetActive(true);
     }
 
-    void OnToggleValueChanged(bool isChecked)
-    {
-        UpdateButtonState(isChecked);
-    }
+    // void OnToggleValueChanged(bool isChecked)
+    // {
+    //     UpdateButtonState(isChecked);
+    // }
 
-    void UpdateButtonState(bool isActive)
-    {
-        myButton.interactable = isActive;
+    // void UpdateButtonState(bool isActive)
+    // {
+    //     myButton.interactable = isActive;
 
-        Image buttonImage = myButton.GetComponent<Image>();
+    //     Image buttonImage = myButton.GetComponent<Image>();
 
-        Color color = buttonImage.color;
-        color.a = isActive ? 1f : 220f / 255f;
-        buttonImage.color = color;
-    }
+    //     Color color = buttonImage.color;
+    //     color.a = isActive ? 1f : 220f / 255f;
+    //     buttonImage.color = color;
+    // }
 
 
 }
